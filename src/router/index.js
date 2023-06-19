4792918157b0c81b072bb958553afb328be2c9bb
yzwuya <111113955+yzwuya@users.noreply.github.com>
2023-06-19 18:04:43 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+  
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
